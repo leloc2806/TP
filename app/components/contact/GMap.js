725e5143cd2d4e808b20182f8b1c53f7ca6447ca
locel <ledinhloc@gmail.com>
2024-06-06 10:36:53 +0700

    fix style product detail

--- app/components/contact/GMap.js
+++ app/components/contact/GMap.js
@@ -1,7 +1,8 @@
 "use client";
 
 export default function GoogleMap ({ center }) {
-  const mapSrc = `https://maps.google.com/maps?q=${center.lat},${center.lng}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
+  // const mapSrc = `https://maps.google.com/maps?q=${center.lat},${center.lng}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
+  const mapSrc = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=${center.lat},${center.lng}`;
 
   return (
     <div>
