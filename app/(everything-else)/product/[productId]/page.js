7f5f564f53eca9d5a562ee5c658e60b46213f6d5
locel <ledinhloc@gmail.com>
2024-06-07 18:00:48 +0700

    add pagination to product category page

--- app/(everything-else)/product/[productId]/page.js
+++ app/(everything-else)/product/[productId]/page.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import flattenAttributes from "@/app/lib/utils";
+import ProductList from "@/app/components/product/ProductList";
 
 export async function generateMetadata({ params }) {
     try {
@@ -64,10 +65,10 @@ export async function generateMetadata({ params }) {
 }
 
 
-async function getProductCategory({params}) {
+async function getProductCategory({ params, page = 1, pageSize = 9 }) {
     try{
         const res = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/api/product-categories?populate=deep,3&filters[slug][$eq]=${params.productId}`, { cache: 'no-store' }
+            `${process.env.NEXT_PUBLIC_API_URL}/api/projects?populate=deep,3&filters[product_category][slug][$eq]=${params.productId}&pagination[page]=${page}&pagination[pageSize]=${pageSize}`, { cache: 'no-store' }
         );
         if (!res.ok) {
             throw new Error("Failed to fetch data");
@@ -84,65 +85,13 @@ async function getProductCategory({params}) {
 
 
 
-export default async function ProductId({params}){
+export default async function ProductId({ params, searchParams }) {
+    const page = searchParams.page || 1;
+    const pageSize = 9;
 
-    const productCategory = await getProductCategory({params});
-
-    const detailData = productCategory.data[0].attributes
-
-    // const jsonLd = {
-    //     '@context': 'https://schema.org',
-    //     '@type': 'Product',
-    //     name: detailData.name,
-    //     image: detailData.image,
-    //     description: detailData.description,
-    // }
+    const data = await getProductCategory({ params, page, pageSize });
 
     return (
-        <>
-            <div className="relative m-0 product-cat-page">
-                <div className="title-page block relative h-auto w-[80vw] max-[1100px]:w-[90vw] mx-auto font-normal text-[5vw] pt-[13rem] px-[0rem] pb-[3rem] max-[1100px]:pt-[160px] max-[1100px]:pb-[20px] max-[580px]:pt-[120px]">
-                    <div className="relative block w-full h-auto overflow-hidden">
-                        <h1 className="text-[4vw] font-normal relative block">{detailData.name}</h1>
-                    </div>   
-                    <span className="absolute bottom-0 left-0 block w-full h-px opacity-60 bg-[var(--color-black20)]"></span>
-                </div>
-
-                <div className="section-outernav">
-                    <div className="outer-nav w-[80vw] max-[1100px]:w-[90vw] mx-auto font-normal text-xl pt-12">
-                        <div className="sub-nav">
-                            <div className="wrap-content min-h-[80vh] block w-[var(--wrapcontent)] m-auto py-[5vw] px-0 relative h-auto z-10">
-                                <div className='load-news-list relative w-full h-auto flex flex-wrap'>
-                                    {detailData.products.data
-                                    .sort((a, b) => new Date(b.attributes.createdAt) - new Date(a.attributes.createdAt))
-                                    .map((product) => ( 
-                                        <Link key={product.id} className='item-product-category relative block' href={`/product/${params.productId}/${product.attributes.slug}`}>
-                                            <div className="product-category-pic relative">
-                                                <div className="wrap-product-category-pic relative">
-                                                    <div className="pic-img relative">
-                                                        {
-                                                            product.attributes.thumbnail.data && product.attributes.thumbnail.data.attributes.url 
-                                                            ? ( <Image src={`${process.env.NEXT_PUBLIC_API_URL}${product.attributes.thumbnail.data.attributes.url}`} alt={product.attributes.title} width={2000} height={1125}/> )
-                                                            : ( <Image src={`${process.env.NEXT_PUBLIC_API_URL}/uploads/image_not_found_0457ab7ad4.jpg`} alt={product.attributes.title} width={2000} height={1125}/> )
-                                                        }
-                                                        
-                                                    </div>
-                                                </div>
-                                                <div className="absolute product-category-text text-[var(--color-white)]">
-                                                    <div className="title-ani ani-item on-show">
-                                                        <h2 className='text-3xl'>{product.attributes.title}</h2>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </Link> 
-                                    ))}
-                                </div>
-                            </div>
-                        </div> 
-                    </div>
-                </div>
-
-            </div>
-        </>
-    )
+        <ProductList params={params} data={data} currentPage={parseInt(page)} pageSize={pageSize} />
+    );
 }
--- app/components/product/ProductList.js
+++ app/components/product/ProductList.js
@@ -15,7 +15,7 @@ const ProductList = ({ params, data, currentPage, pageSize }) => {
     const handlePageChange = async (page) => {
         setIsLoading(true);
         const res = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/api/product-categories?populate=deep,3&filters[slug][$eq]=${params.productId}&pagination[page]=${page}&pagination[pageSize]=${pageSize}`, 
+            `${process.env.NEXT_PUBLIC_API_URL}/api/projects?populate=deep,3&filters[product_category][slug][$eq]=${params.productId}&pagination[page]=${page}&pagination[pageSize]=${pageSize}`, 
             { cache: 'no-store' }
         );
         const newData = await res.json();
@@ -28,13 +28,14 @@ const ProductList = ({ params, data, currentPage, pageSize }) => {
         return <div>Loading...</div>;
     }
 
-    const detailData = productCategory.data[0].attributes;
+    const detailCategoryData = productCategory.data[0].attributes.product_category.data.attributes;
+    const detailData = productCategory.data
 
     return (
         <div className="relative m-0 product-cat-page">
             <div className="title-page block relative h-auto w-[80vw] max-[1100px]:w-[90vw] mx-auto font-normal text-[5vw] pt-[13rem] px-[0rem] pb-[3rem] max-[1100px]:pt-[160px] max-[1100px]:pb-[20px] max-[580px]:pt-[120px]">
                 <div className="relative block w-full h-auto overflow-hidden">
-                    <h1 className="text-[4vw] font-normal relative block">{detailData.name}</h1>
+                    <h1 className="text-[4vw] font-normal relative block">{detailCategoryData.name}</h1>
                 </div>
                 <span className="absolute bottom-0 left-0 block w-full h-px opacity-60 bg-[var(--color-black20)]"></span>
             </div>
@@ -44,7 +45,7 @@ const ProductList = ({ params, data, currentPage, pageSize }) => {
                     <div className="sub-nav">
                         <div className="wrap-content min-h-[80vh] block w-[var(--wrapcontent)] m-auto py-[5vw] px-0 relative h-auto z-10">
                             <div className='load-news-list relative w-full h-auto flex flex-wrap'>
-                                {detailData.products.data
+                                {detailData
                                     .sort((a, b) => new Date(b.attributes.createdAt) - new Date(a.attributes.createdAt))
                                     .map((product) => (
                                         <Link key={product.id} className='item-product-category relative block' href={`/product/${params.productId}/${product.attributes.slug}`}>
@@ -61,17 +62,23 @@ const ProductList = ({ params, data, currentPage, pageSize }) => {
                                                 </div>
                                                 <div className="absolute product-category-text text-[var(--color-white)]">
                                                     <div className="title-ani ani-item on-show">
-                                                        <h2 className='text-3xl'>{product.attributes.title}</h2>
+                                                        <h2 className='text-3xl leading-normal'>{product.attributes.title}</h2>
                                                     </div>
                                                 </div>
                                             </div>
                                         </Link>
                                     ))}
                             </div>
-                            <div className="pagination">
-                                <button disabled={currentPage <= 1} onClick={() => handlePageChange(currentPage - 1)}>Previous</button>
-                                <span>Page {currentPage}</span>
-                                <button disabled={currentPage >= totalPages} onClick={() => handlePageChange(currentPage + 1)}>Next</button>
+                            <div className="pagination relative my-[20px]">
+                            {[...Array(totalPages)].map((_, index) => (
+                                <button
+                                    key={index}
+                                    onClick={() => handlePageChange(index + 1)}
+                                    className={`pagination-bullet ${index + 1 === currentPage ? 'pagination-bullet-active' : ''}`}
+                                >
+                                    {index < 9 ? `0${index + 1}` : index + 1}
+                                </button>
+                            ))}
                             </div>
                         </div>
                     </div>
